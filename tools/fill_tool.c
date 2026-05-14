38ec51594937b87a304f26a22aa3b1243eb666cb
eddieburke <eddiesgotmail@yahoo.com>
2026-05-14 16:32:45 -0400

    Simplify fill tool dispatch

--- tools/fill_tool.c
+++ tools/fill_tool.c
@@ -4,22 +4,17 @@
 #include "../floodfill.h"
 #include "../helpers.h"
 #include "../history.h"
-#include "../layers.h"
 
 void FillToolOnMouseDown(HWND hWnd, int x, int y, int nButton) {
-  if (nButton != MK_LBUTTON && nButton != MK_RBUTTON) {
-    return;
-  }
+  (void)hWnd;
 
-  HBITMAP hOldColor = NULL;
-  HDC hColor = LayersGetActiveColorDC(&hOldColor);
-  if (hColor) {
-    BOOL changed = FloodFillCanvas(x, y, GetColorForButton(nButton), 255);
-    ReleaseBitmapDC(hColor, hOldColor);
-    if (changed) {
-      HistoryPushToolActionById(TOOL_FILL, "Flood Fill");
-      InvalidateCanvas();
-      SetDocumentDirty();
-    }
-  }
+  if (nButton != MK_LBUTTON && nButton != MK_RBUTTON)
+    return;
+
+  if (!FloodFillCanvas(x, y, GetColorForButton(nButton), 255))
+    return;
+
+  HistoryPushToolActionById(TOOL_FILL, "Flood Fill");
+  InvalidateCanvas();
+  SetDocumentDirty();
 }
