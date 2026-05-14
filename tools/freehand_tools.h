b9b2c61ac6a6f8fc30e6972cc3c39e2baf7d0f94
eddieburke <eddiesgotmail@yahoo.com>
2026-05-14 16:41:08 -0400

    Keep legacy freehand dispatch hooks for airbrush routing

--- tools/freehand_tools.h
+++ tools/freehand_tools.h
@@ -2,10 +2,14 @@
 #define FREEHAND_TOOLS_H
 
 #include "../peztold_core.h"
+#include "airbrush_tool.h"
 
 void FreehandTool_OnMouseDown(HWND hWnd, int x, int y, int nButton, int toolId);
 void FreehandTool_OnMouseMove(HWND hWnd, int x, int y, int nButton, int toolId);
 void FreehandTool_OnMouseUp(HWND hWnd, int x, int y, int nButton, int toolId);
+
+/* Legacy dispatch hook kept so tools.c can continue routing timer events. */
+void FreehandTool_OnTimerTick(void);
 
 BOOL IsFreehandDrawing(void);
 void FreehandTool_Deactivate(void);
